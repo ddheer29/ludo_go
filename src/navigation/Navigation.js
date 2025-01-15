be5c571617d94511833e5543e005a5678aac2b98
ddheer29 <divyangdheer11@gmail.com>
2025-01-15 22:23:14 +0530

    navigation setup

---
+++ src/navigation/Navigation.js
@@ -0,0 +1,48 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import LudoBoardScreen from '../screens/LudoboardScreen';
+import HomeScreen from '../screens/HomeScreen';
+import SplashScreen from '../screens/SplashScreen';
+import {navigationRef} from '../helper/NavigationUtil';
+
+const Stack = createNativeStackNavigator();
+
+const Navigation = () => {
+  return (
+    <NavigationContainer ref={navigationRef}>
+      <Stack.Navigator
+        initialRouteName="LudoBoard"
+        screenOptions={() => ({
+          headerShown: false,
+        })}>
+        <Stack.Screen
+          name="LudoBoard"
+          component={LudoBoardScreen}
+          options={{
+            animation: 'fade',
+          }}
+        />
+        <Stack.Screen
+          name="HomeScreen"
+          component={HomeScreen}
+          options={{
+            animation: 'fade',
+          }}
+        />
+        <Stack.Screen
+          name="SplashScreen"
+          component={SplashScreen}
+          options={{
+            animation: 'fade',
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Navigation;
+
+const styles = StyleSheet.create({});
